cebac6b616bb6094686d3e75c78b88ea29bd7918
phil04786 <abmi04786@gmail.com>
2025-06-01 23:50:07 +0530

    new change at 23:50

---
+++ 18 - Practice Project Building a Food Order App/src/store/CartContext.jsx
@@ -0,0 +1,80 @@
+import { createContext, useReducer } from "react";
+
+const CartContext = createContext({
+  items: [],
+  addItem: (item) => {},
+  removeItem: (id) => {},
+});
+
+function cartReducer(state, action) {
+  if (action.type === "ADD_ITEM") {
+    // state.items.push(action.item); -> Directly Manuplating the Array and we want to increase or decrease item quantity rather adding then adding item quantity.
+    const existingCartItemIndex = state.items.findIndex(
+      (item) => item.id === action.item.id
+    );
+
+    const updatedItems = [...state.items];
+
+    //item exist in the item array otherwise it doesn't
+    if (existingCartItemIndex > -1) {
+      const existingItem = state.items[existingCartItemIndex];
+      const updatedItem = {
+        ...existingItem,
+        quantity: existingItem.quantity + 1,
+      };
+      updatedItems[existingCartItemIndex] = updatedItem;
+    } else {
+      updatedItems.push({ ...action.item, quantity: 1 });
+    }
+
+    return { ...state, items: updatedItems };
+  }
+
+  if (action.type === "REMOVE_ITEM") {
+    const existingCartItemIndex = state.items.findIndex(
+      (item) => item.id === action.id //action.item.id
+    );
+
+    const existingCartItem = state.items[existingCartItemIndex];
+
+    const updatedItems = [...state.items];
+
+    if (existingCartItem.quantity === 1) {
+      updatedItems.splice(existingCartItemIndex, 1);
+    } else {
+      const updatedItem = {
+        ...existingCartItem,
+        quantity: existingCartItem.quantity - 1,
+      };
+      updatedItems[existingCartItemIndex] = updatedItem;
+    }
+    return { ...state, items: updatedItems };
+  }
+
+  return state;
+}
+
+export function CartContextProvider({ children }) {
+  const [cart, dispatchCartAction] = useReducer(cartReducer, { items: [] });
+
+  function addItem(item) {
+    dispatchCartAction({ type: "ADD_ITEM", item: item });
+  }
+
+  function removeItem(id) {
+    dispatchCartAction({ type: "REMOVE_ITEM", id });
+  }
+
+  const cartContext = {
+    items: cart.items,
+    addItem: addItem,
+    removeItem: removeItem,
+  };
+
+  console.log(cartContext);
+  return (
+    <CartContext.Provider value={cartContext}>{children}</CartContext.Provider>
+  );
+}
+
+export default CartContext;
